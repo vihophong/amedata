bd646b2e030d002d56a2b1620db74845f4bc4a8e
vihophonghus <vihophong@hus.edu.vn>
2021-07-07 17:14:58 +0700

    completed scripts to add new ame 2020 data to the exisiing delayedn evaluation

--- getmass.py
+++ getmass.py
@@ -7,7 +7,7 @@ data1 = []
 iline = 0
 flagread = 0
 flagreadfirst = 0
-print("N-Z,N,Z,A,EL,O,Mass_excess_keV,D_Mass_excess_keV,Binding_enrgy_perA_keV,D_Binding_enrgy_perA_keV,Decay_mode,Qbeta_keV,D_Qbeta_keV,A,Atomic_mass_microu,D_Atomic_mass_microu,end")
+print("N-Z,N,Z,A,EL,O,Mass_excess_keV,D_Mass_excess_keV,is_from_experiment,Binding_enrgy_perA_keV,D_Binding_enrgy_perA_keV,is_from_experiment,Decay_mode,Qbeta_keV,D_Qbeta_keV,is_from_experiment,A,Atomic_mass_microu,D_Atomic_mass_microu,is_from_experiment")
 while True:
 	iline += 1
 	line = file1.readline()
@@ -46,18 +46,25 @@ while True:
 			columnitems.append("999999")
 		#print items[i],
 	#print("")
+	is_experiment = [1] * 16
 	for i in range(len(columnitems)):
 		if (columnitems[i]=="*"):
 			columnitems[i] = "999999"
 		if (columnitems[i][len(columnitems[i])-1]=="#"):
 			columnitems[i] = columnitems[i][:len(columnitems[i])-1]
-	for i in range(len(columnitems)):
-		print(columnitems[i],end=',')
-	print("end")
+			is_experiment[i]=0
+
+
 	data1.append({"N-Z": int(columnitems[0]),"N": int(columnitems[1]),"Z": int(columnitems[2]),"A": int(columnitems[3]),"EL": str(columnitems[4]),
-		"O": str(columnitems[5]),"M": float(columnitems[6]),"DM": float(columnitems[7]),"B": float(columnitems[8]),
-		"DB": float(columnitems[9]), "Decay": str(columnitems[10]),"Qb": float(columnitems[11]),
-		"DQb": float(columnitems[12]), "A2": int(columnitems[13]), "AM": float(columnitems[14]),"DAM": float(columnitems[15]) })
+		"O": str(columnitems[5]),"M": float(columnitems[6]),"DM": float(columnitems[7]),"is_ex_M": is_experiment[6],"B": float(columnitems[8]),
+		"DB": float(columnitems[9]),"is_ex_B": is_experiment[8],"Decay": str(columnitems[10]),"Qb": float(columnitems[11]),
+		"DQb": float(columnitems[12]),"is_ex_Qb": is_experiment[11],"A2": int(columnitems[13]), "AM": float(columnitems[14]),"DAM": float(columnitems[15]),"is_ex_AM": is_experiment[14] })
+
+
+for i in range(len(data1)):
+	print("{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}".format(data1[i]["N-Z"],data1[i]["N"],data1[i]["Z"],data1[i]["A"],
+		data1[i]["EL"],data1[i]["O"],data1[i]["M"],data1[i]["DM"],data1[i]["is_ex_M"],data1[i]["B"],data1[i]["DB"],data1[i]["is_ex_B"],
+		data1[i]["Decay"],data1[i]["Qb"],data1[i]["DQb"],data1[i]["is_ex_Qb"],data1[i]["A2"],data1[i]["AM"],data1[i]["DAM"],data1[i]["is_ex_AM"]))
 
 np.save("mass_1.mas20.npy",data1)
 
